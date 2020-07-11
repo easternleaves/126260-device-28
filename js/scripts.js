55c645b9f7f6f630cd1197a20b3c852ff72e2324
easternleaves <idmitrydsgn@gmail.com>
2020-07-11 18:34:35 +0300

    Оживил карту

--- js/scripts.js
+++ js/scripts.js
@@ -56,3 +56,27 @@ window.addEventListener("keydown", function (evt) {
     }
   }
 });
+
+var mapLink = document.querySelector(".map-preview");
+var mapPopup = document.querySelector(".modal-map");
+var mapClose = mapPopup.querySelector(".modal-close");
+
+mapLink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mapPopup.classList.add("modal-show");
+});
+
+mapClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mapPopup.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if(evt.keyCode === 27) {
+    if(mapPopup.classList.contains("modal-show")) {
+      evt.preventDefault();
+      mapPopup.classList.remove("modal-show");
+    }
+  }
+});
+
